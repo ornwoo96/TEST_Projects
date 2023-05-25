06275287aea251fd7409c2739adcecd6a8b5922b
ornwoo96 <dongou705@naver.com>
2023-05-25 15:06:45 +0900

    test

--- TestProject/MainViewController.swift
+++ TestProject/MainViewController.swift
@@ -7,10 +7,47 @@
 
 import UIKit
 
-class MainViewController: UIViewController {
+struct Company {
+    let direction: Int
+    let person: [Person]
+}
+
+struct Person {
     
+}
+
+class MainViewController: UIViewController {
+    lazy var collectionView: UICollectionView = {
+        let 
+        
+        let collectionView = UICollectionView(frame: .zero, collectionViewLayout: <#T##UICollectionViewLayout#>)
+        collectionView.dataSource = self
+        collectionView.delegate = self
+        return collectionView
+    }()
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        setupUI()
     }
+    
+    private func setupUI() {
+        
+    }
+}
+
+extension MainViewController: UICollectionViewDataSource {
+    func collectionView(_ collectionView: UICollectionView,
+                        numberOfItemsInSection section: Int) -> Int {
+        <#code#>
+    }
+    
+    func collectionView(_ collectionView: UICollectionView,
+                        cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        <#code#>
+    }
+}
+
+extension MainViewController: UICollectionViewDelegate {
+    
 }
